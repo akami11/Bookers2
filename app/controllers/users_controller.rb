0e4d2535a8b27006598d3e5b38eafc3da67fb3ce
akami11 <cerojp24@gmail.com>
2021-02-17 04:55:22 +0000

    Initial Commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,44 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!,except: [:top]
+
+  def new
+    @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
+    @book = Book.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    if @user == current_user
+      render :edit
+    else
+      redirect_to user_path(current_user)
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "successfully User Update!!"
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '54fe3c5581fa9e7ac83c172f076a3b42d55d41eed1acf7118ed0e5d5c8c08d12f32f9981285a8c1695ff8353dfcd21bd2441310d58297f8f988e18dc0b7e943e'
